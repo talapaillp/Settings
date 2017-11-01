98f1ae2624e21155897be409edbb470b33642275
Cristian Tabacitu <cristitabacitu@gmail.com>
2017-11-01 08:00:16 +0000

    Apply fixes from StyleCI

--- src/resources/lang/en/settings.php
+++ src/resources/lang/en/settings.php
@@ -9,10 +9,10 @@ return [
     | The following language lines are used for Laravel Backpack - Settings
     |
     */
-    'name'               => 'Name',
-    'value'              => 'Value',
-    'description'        => 'Description',
-    'setting_singular'   => 'setting',
-    'setting_plural'     => 'settings',
+    'name'             => 'Name',
+    'value'            => 'Value',
+    'description'      => 'Description',
+    'setting_singular' => 'setting',
+    'setting_plural'   => 'settings',
 
 ];
--- src/resources/lang/pt/settings.php
+++ src/resources/lang/pt/settings.php
@@ -9,10 +9,10 @@ return [
   | The following language lines are used for Laravel Backpack - Settings
   |
   */
-  'name'               => 'Nome',
-  'value'              => 'Valor',
-  'description'        => 'Descrição',
-  'setting_singular'   => 'configuração',
-  'setting_plural'     => 'configurações',
+  'name'             => 'Nome',
+  'value'            => 'Valor',
+  'description'      => 'Descrição',
+  'setting_singular' => 'configuração',
+  'setting_plural'   => 'configurações',
 
 ];
